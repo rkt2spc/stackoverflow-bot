63940146e903903202fc6981abf027a16ae7518b
Tuan Nguyen <nmtuan.dev@gmail.com>
2018-07-26 04:16:51 +0700

    Split env.sh from setup.sh so it can be source during cron-job

---
+++ env.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Don't edit these
+export DISPLAY=:9.0
+
+######################################
+# Configurable environment variables #
+######################################
+# Reporting Config
+export REPORT_TO_EMAIL='<fill-me>'
+
+# StackOverflow Config
+export STACKOVERFLOW_EMAIL='<fill-me>'
+export STACKOVERFLOW_PASSWORD='<fill-me>'
+
+# SMTP Config
+export SMTP_HOST='<fill-me>'
+export SMTP_PORT='<fill-me>'
+export SMTP_USERNAME='<fill-me>'
+export SMTP_PASSWORD='<fill-me>'
--- setup.sh
+++ setup.sh
@@ -8,20 +8,3 @@ apt install --yes \
 
 # Start Xvfb
 Xvfb -ac -screen scrn 1280x2000x24 :9.0
-export DISPLAY=:9.0
-
-######################################
-# Configurable environment variables #
-######################################
-# Reporting Config
-export REPORT_TO_EMAIL='<fill-me>'
-
-# StackOverflow Config
-export STACKOVERFLOW_EMAIL='<fill-me>'
-export STACKOVERFLOW_PASSWORD='<fill-me>'
-
-# SMTP Config
-export SMTP_HOST='<fill-me>'
-export SMTP_PORT='<fill-me>'
-export SMTP_USERNAME='<fill-me>'
-export SMTP_PASSWORD='<fill-me>'
